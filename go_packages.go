9d298c41e987a30e37594306023bf9d847a8b1ff
Azizbek Gaybullaev <azizgaybullaev@mail.ru>
2020-01-17 18:37:03 +0500

    added changes

--- go_packages.go
+++ go_packages.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"GoTutorials/rectangle"
+	"fmt"
+	"log"
+)
 //Packages are used to organise go source code for better reusability and readability.
 
+var rectLen, recWidth float64 = 6, 7
+
+func init()  {
+	println("main package initialized")
+	if rectLen < 0 {
+		log.Fatal("length is less than zero")
+	}
+	if recWidth < 0 {
+		log.Fatal("width is less than zero")
+	}
+}
+
+func main()  {
+	fmt.Println("Go packages in this tutorial")
+	fmt.Printf("area pf rectangle %.2f\n", rectangle.Area(rectLen, recWidth))
+	fmt.Printf("area pf rectangle %.2f\n", rectangle.Diagonal(rectLen, recWidth))
+}
---
+++ rectangle/rectprops.go
@@ -0,0 +1,21 @@
+package rectangle
+
+import (
+	"fmt"
+	"math"
+)
+
+func init()  {
+	fmt.Println("rectangle package initialised")
+}
+
+func Area(len, wid float64) float64 {
+	area := len * wid
+	return area
+}
+
+func Diagonal(len, wid float64) float64 {
+	diagonal := math.Sqrt((len * len) + (wid * wid))
+
+	return diagonal
+}
